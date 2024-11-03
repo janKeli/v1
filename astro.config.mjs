2893cc0f4ac079066275ac5c1e897f14d11e54a4
jan Keli <janelikan@proton.me>
2024-11-03 08:30:20 +0000

    refactor: change name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://janElikan.github.io',
+  site: 'https://jankeli.github.io',
   markdown: {
     shikiConfig: {
       theme: 'catppuccin-mocha'
