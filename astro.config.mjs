33b4d8f6ec6fa62dd5bb2caf3e008f228c1cced4
jan Keli <janelikan@proton.me>
2024-12-15 13:43:52 +0000

    fix: base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jankeli.github.io/v1',
+  site: 'https://jankeli.github.io',
+  base: '/v1',
   markdown: {
     shikiConfig: {
       theme: 'catppuccin-mocha'
